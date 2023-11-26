7bf8fa919e6742e724570a5b2f4a73ff99974f25
Haile <95925523+haile1713@users.noreply.github.com>
2023-11-26 22:07:13 +0300

    Add files via upload
    tadaaaa

---
+++ Mad Libs/madlibs.py
@@ -0,0 +1,14 @@
+noun = input ("someone's name: ")
+adjective = input ("adjective: ")
+verb= input ("verb: ") 
+body_part= input ("part of a body ") 
+number= input ("number: ") 
+noun2 = input ("noun: ")
+adverb = input ("adverb: ")
+verb2 = input ("verb: ")
+plural_pronoun = input ("plural-pronoun: ")
+noun3 = input ("ur name : ")
+
+story= f" Dear {noun},You are extremly {adjective} and I {verb} you! I want kiss your {body_part} {number} times. You make my {noun2} burn with desire. When I first saw you, I {adverb} stared at you and fell in love. Will you {verb2} out with me? Don`t let your parents discourage you, {plural_pronoun} are just jealous.Yours forever,{noun3}. "
+
+print (story)
