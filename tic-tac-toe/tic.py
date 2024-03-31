b9c0834e239c077290f70b22c2673fa0801a4934
haile <haileleulfiseha@gmail.com>
2024-03-31 15:02:29 +0300

    done!!

--- tic-tac-toe/tic.py
+++ tic-tac-toe/tic.py
@@ -1,87 +1,117 @@
-board =[
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
 ]
 
-# print(board)
+user =True
+turns =0
 
 def print_board(board):
     for row in board:
-        for slot in row :
-            print(slot + " " , end="")
+        for slot in row:
+            print(slot + " ", end="")
         print()
 
-# print_board(board)
-
-def quit(user_quit):
-
-    if user_input.lower() =="q":
-        print("thanks for playing")
+def quit_game(user_input):
+    if user_input.lower() == "q":
+        print("Thanks for playing")
         return True
     else:
         return False
-
 
 def check_input(user_input):
-    if not isnum(user_input):
-        return False
-    user_input = int(user_input)
-    
-    if not check_1_9(user_input): return False
-    return True
-def check_1_9(user_input):
-    if user_input >9 or user_input <1:
-        print ("number out of range")
-        return False
-
-
-    else:
-        return True
-
-
-def isnum(user_input):
     if not user_input.isnumeric():
-        print("Invalid input ") 
+        print("Invalid input, please enter a number from 1 to 9")
         return False
-    else:
-        return True
 
-def istaken(coords,board):
-    row = coords[0]
-    col = coords[1]
+    user_input = int(user_input)
+    if not 1 <= user_input <= 9:
+        print("Number out of range, please enter a number from 1 to 9")
+        return False
 
+    return True
+
+def is_taken(coords, board):
+    row, col = coords
     if board[row][col] != "-":
-        print("position taken")
+        print("Position already taken, please choose another")
         return True
-    else :
+    else:
         return False
 
-def coordinates(user_input):
-    row = int(user_input/3)
-    col= user_input
-    if col >2 :
-        col=int(col%3)
-    return (row,col)
+def get_coordinates(user_input):
+    user_input = int(user_input) - 1
+    row = user_input // 3
+    col = user_input % 3
+    return row, col
 
-def add_to_board(coords,board):
-    row =coords[0] 
-    col = coords[1]
+def add_to_board(coords, board,active_user):
+    row, col = coords
+    board[row][col] = active_user
 
-    board[row][col] = "x"
+def current_user(user):
+    if user:
+        return "x"
+    else :
+        return "O"
 
-while True:
+def iswin(user,board):
+
+    if check_row(user,board):   
+         return True   
+    if check_col (user,board):
+        return True
+    if check_diag (user,board):
+        return True
+    return False
+def check_row(user,board):
+    for row in board:
+        complete_row = True
+        for slot in row:
+            if slot != user:
+                complete_row = False
+                break
+        if complete_row : return True
+    return False
+
+
+def check_col(user,board):
+    
+    for col in range(3):
+        complete_col = True
+        for row in range(3):
+            if board[row][col] != user:
+                complete_col = False
+                break
+        if  complete_col : return True
+    return False 
+
+
+def check_diag(user,board):
+    if board [0][0] == user and  board [1][1] == user and board[2][2] ==user:
+        return True
+    elif board[0][2] == user and board [1][1] ==user and board [2][0] == user:
+        return True
+    else: return False
+
+while turns <9 :
+    active_user=current_user(user)
     print_board(board)
-    user_input=input("Enter a position 1-9: or enter \"q\" to quit:")
-    if quit(user_input):
+    user_input = input("Enter a position 1-9, or enter 'q' to quit: ")
+    if quit_game(user_input):
         break
     if not check_input(user_input):
-        print("try again")
         continue
-    user_input= int(user_input)-1
-    coords=coordinates(user_input)
-    # board[0][0]="x"
-    if istaken (coords,board):
-        print (" try again")
+    row, col = get_coordinates(user_input)
+    if is_taken((row, col), board):
         continue
-    
+    add_to_board((row, col), board,active_user)
+
+    if iswin(active_user,board):
+        print (f"{active_user.upper()} Won!")
+        break
+    turns +=1
+    if turn ==9 :
+        print ("draw!")
+    user= not user
