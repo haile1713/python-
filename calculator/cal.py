d7d32748ac2d5dec01e4cc2f963e8fd4248acbd2
Haile <95925523+haile1713@users.noreply.github.com>
2024-03-31 12:05:37 +0300

    Add files via upload

---
+++ calculator/cal.py
@@ -0,0 +1,13 @@
+num1= int(input("input ur first number :"))
+num2= int(input("input ur second number :"))
+operator = input("Specify the math operation here (+, -, *, /): ")
+
+if operator == "+":
+    print (num1+num2)
+elif operator == "-":
+    print (num1-num2)
+elif operator == "*":
+    print (num1*num2)
+elif operator == "/":
+    print (num1/num2)
+
